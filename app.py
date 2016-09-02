f69b084a5ae83c735971ee05f7d17150e1d87692
Mohamed <mohamed.essam.arafa@gmail.com>
2016-09-02 19:26:10 +0200

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/heil')
+@application.route('/heil')
 def fu():
     return "Heil %s" % request.args.get("name")
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
-from app import app
-
-application = app
+from app import application
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
